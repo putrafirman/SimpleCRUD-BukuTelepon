a89a0b8e9623e91ce7cec50e99922650e7dccd79
Putra Firman Ardiansyah <ardidroid97@gmail.com>
2017-03-11 02:32:45 +0700

    clean code edit_proses.php

--- edit_proses.php
+++ edit_proses.php
@@ -1,16 +1,12 @@
 <?php
-// mengecek apakah tombol edit telah diklik
 if (isset($_POST['edit'])) {
-  // buat koneksi dengan database
   include("koneksi.php");
 
-  // membuat variabel untuk menampung data dari form edit
   $id = $_POST['id'];
 	$nama	= $_POST['nama'];
 	$notelepon	= $_POST['notelepon'];
 	$email = $_POST['email'];
 
-  //buat dan jalankan query UPDATE
   $query  = "UPDATE bukutelepon SET ";
   $query .= "nama = '$nama', ";
   $query .= "notelepon='$notelepon', ";
@@ -19,14 +15,12 @@ if (isset($_POST['edit'])) {
 
   $result = mysqli_query($link, $query);
 
-  //periksa hasil query apakah ada error
   if(!$result) {
     die ("Query gagal dijalankan: ".mysqli_errno($link).
        " - ".mysqli_error($link));
   }
 }
 
-//lakukan redirect ke halaman index.php
 header("location:index.php");
 
 
